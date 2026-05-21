7a64071118a4ded82fa81c45609b4afbeeda7950
Ahmed Zakaria <ahmzak99@gmail.com>
2026-05-21 10:55:52 +0300

    chore: add vue 3 with vite plugin

---
+++ resources/js/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <div class="p-6">
+        <h1 class="text-2xl font-semibold">Winch</h1>
+        <p class="mt-2 text-gray-600">Vue 3 is ready.</p>
+    </div>
+</template>
